2bc9c5f0cd4d4f496730afc164d1c46037f11890
Mark Lumba <Mark.Lumba.-ND@disney.com>
2026-02-16 12:29:10 -0500

    add fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,30 +8,30 @@ const HomePage: React.FC = () => {
       </section>
       <section
         id="aboutme"
-        className="h-[100vh] bg-lime-500 flex items-center justify-center"
+        className=" bg-lime-500 h-[100vh] flex relative items-center justify-center pb-3"
       >
         <div className="container flex flex-col gap-10 items-center p-5">
-          <h1 className="bg-white text-5xl p-1 font-semibold w-fit text-lime-500">
+          <h1 className="bg-white text-6xl md:text-8xl px-5 font-semibold w-fit text-lime-500 z-20">
             About Me
           </h1>
-          <div className="flex gap-10">
-            <div className="w-1/3 hidden md:block">
+          <div className="flex flex-col md:flex-row items-center gap-10">
+            <div className="w-1/3 hidden lg:block">
               <Image src={"/me.jpg"} width={400} height={400} alt="person" />
             </div>
-            <div className="w-3/3 md:w-2/3 text-xl font-semibold text-white flex flex-col gap-5 text-justify">
+            <div className="w-3/3 lg:w-2/3 text-xl font-semibold text-white flex flex-col gap-5 text-justify">
               <p>
                 I like combining simple design with practical coding. I enjoy
                 working where design meets development — making websites that
                 look nice, run smoothly, and are easy for everyone to use.
               </p>
-              <p>
+              <p className="hidden md:block">
                 Currently, I’m a Consultant III at RCG Global Services, working
                 extensively with Adobe Experience Manager (AEM) and React.js. I
                 build reusable AEM React components and collaborate closely with
                 UI/UX designers to turn creative designs into functional,
                 accessible web pages that meet business goals.
               </p>
-              <p>
+              <p className="hidden md:block">
                 Previously, I’ve contributed to projects at Accenture and
                 various government organizations, developing scalable frontend
                 applications using Vue, React, and modern JavaScript
